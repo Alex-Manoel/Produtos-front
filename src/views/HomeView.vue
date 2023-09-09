e6a32550cf7345e0e578984315d87f914ffdaca0
Alex MS <@alexmanoelsilva100>
2023-09-09 00:41:20 -0300

    Front-inicio

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ListaProdutos/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ListaProdutos from '@/components/Produtos/ListaProdutos.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    ListaProdutos
   }
 }
 </script>
